30c0325fae849ae096e3855b8fd0cab9b201b2cc
aev <aev41000@gmail.com>
2025-06-10 18:20:37 +0200

    func leer_archivo

--- analisis_datos/filtrado.py
+++ analisis_datos/filtrado.py
@@ -1,5 +1,22 @@
 import pandas as pd
+import os
 
-ruta = '../excel/Respuestas_linea5.xlsx'
-# Lee la primera hoja por defecto
-df = pd.read_excel(ruta)
+
+def leer_archivo(archivo_path):
+    ext = os.path.splitext(archivo_path)[1].lower() # Obtener la extensión del archivo
+    
+    if ext in ['.xlsx', '.xls']:
+        return pd.read_excel(archivo_path, engine='openpyxl') # Necesita `openpyxl`
+    elif ext == '.csv':
+        return pd.read_csv(archivo_path)
+    elif ext == '.ods':
+        return pd.read_excel(archivo_path, engine='odf')  # Necesita `odfpy` instalado
+    else:
+        raise ValueError("Formato de archivo no soportado")
+
+
+
+
+if __name__ == "__main__":
+    ruta = 'MatrticesSmart/excel/Respuestas_linea5.xlsx'
+    df = leer_archivo(ruta)
