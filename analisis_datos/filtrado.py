429af77b30b53ade482ed5bcce80bc99e425a906
aev <aev41000@gmail.com>
2025-06-10 17:48:07 +0200

    Modificacion datos de ejemplo

--- analisis_datos/filtrado.py
+++ analisis_datos/filtrado.py
@@ -1,40 +1,5 @@
 import pandas as pd
 
-ruta = './excel/Linea 5_Promover el acceso a la vivienda. (respuestas).xlsx'
+ruta = '../excel/Respuestas_linea5.xlsx'
 # Lee la primera hoja por defecto
 df = pd.read_excel(ruta)
-
-criterios = ["Importancia", "Urgencia", "Esfuerzo organizativo", "Impacto"]
-
-# Seleccionamos solo las columnas que contienen esos criterios
-cols_relevantes = [col for col in df.columns if any(c in col for c in criterios)]
-df_filtrado = df[cols_relevantes]
-
-escala = {
-    "Muy en desacuerdo": 1,
-    "En desacuerdo": 2,
-    "De acuerdo": 3,
-    "Muy de acuerdo": 4,
-    "Si": 3,
-    "No": 1,
-}
-
-# Aplica el mapeo
-df_numerico = df_filtrado.replace(escala)
-
-temas = {
-    "Viviendas protección oficial": [col for col in df.columns if "viviendas de protección oficial" in col],
-    "Vivienda vacía": [col for col in df.columns if "vivienda vacía" in col],
-    "Gravar viviendas desocupadas": [col for col in df.columns if "grabar las viviendas desocupadas" in col],
-    "Cooperación asociaciones": [col for col in df.columns if "cooperación entre las diferentes asociaciones" in col],
-    "Bolsa de viviendas": [col for col in df.columns if "bolsa de viviendas" in col]
-}
-
-
-resultados = {}
-for tema, columnas in temas.items():
-    puntuacion = df[columnas].replace(escala).mean(axis=1).values[0]  # solo 1 fila
-    resultados[tema] = puntuacion
-
-
-print(resultados)
